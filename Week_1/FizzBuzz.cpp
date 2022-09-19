362225d1d7db985f89636998638d24c5a045dcf0
bazabizi <bazabizi3@gmail.com>
2022-09-19 20:02:10 +0300

    first

---
+++ Week_1/FizzBuzz.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        
+        vector <string>x;
+        string a;
+        for (int i=1;i<=n;i++)
+        {
+            if(i%3==0 && i%5==0){
+                a="FizzBuzz";
+            }
+            else if(i%3==0){
+                a="Fizz";
+            }    
+            else if(i%5==0){
+                a="Buzz";
+            }
+            else {
+                a=to_string(i);
+            }
+           x.push_back(a); 
+            
+        }
+        return x;
+    }
+};
