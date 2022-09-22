856c2d921932d51f1d69769dca601f9cd6d4b2e7
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2022-09-22 06:56:10 +0300

    Update theatre.cpp

--- Week_1/theatre.cpp
+++ Week_1/theatre.cpp
@@ -1,7 +1,13 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-int main(){ 
-    double x, y, z; 
-    cin >>x>>y>>z; 
-    cout<<(long long)ceil(x/z)*(long long)ceil(y/z)<<endl; 
-} 
+#include <bits/stdc++.h>
+using namespace std;
+double Theatre(double x,double y,double z)
+{
+	return ceil(x/z)*ceil(y/z);
+}
+int main ()
+{
+	double x,y,z;
+	cin>>x>>y>>z;
+	cout<<(long long)Theatre(x,y,z);
+	return 0;
+}
