95e9e4b2af70b40324c451c5f4357e751ef1d94c
bazabizi <bazabizi3@gmail.com>
2022-10-03 15:17:31 +0300

    done

---
+++ week_3/max consecutive ones III.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int i=0;
+        int j=0;
+        int ans=0;
+        int count=0;
+        for(j=0;j<nums.size();j++)
+        {
+            if(nums[j] == 0)
+                 count++;
+            while (count > k)
+            {
+                if (nums[i] == 0)
+                    count--;
+                i++;
+            }
+            ans = max(ans, (j -i + 1));
+        }
+        return ans;
+    }
+};
