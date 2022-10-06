d065e7f1627e8a706a31aecb5227510223dbb151
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2022-10-06 12:59:26 +0300

    done

--- week_3/move zeros.cpp
+++ week_3/move zeros.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        int temp=0;
         for(int i=0;i<nums.size();i++)
         {
-            for(int j=i+1;j<nums.size();j++)
+            if(nums[i]!=0)
             {
-                if(nums[i]==0)
-                {
-                    swap(nums[i],nums[j]);
-                }
+                nums[temp]=nums[i];
+                temp++;
             }
         }
+        while(temp<nums.size())
+        {
+            nums[temp]=0;
+            temp++;
+        }
+        
     }
 };
